f3e0b93dd01ee51d2a234f0d84c1b09a071b4cf8
Alex Richardson <alex@alexcr.com>
2024-12-02 10:46:07 -0500

    day 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { p1, p2 } from './days/1';
+import { p1 as dayOneP1, p2 as dayOneP2 } from './days/1';
+import { p1 as dayTwoP1, p2 as dayTwoP2 } from './days/2';
 
 console.log('Day One:');
-console.log(p1());
-console.log(p2());
+console.log(dayOneP1());
+console.log(dayOneP2());
+
+console.log('Day Two:');
+console.log(dayTwoP1());
+console.log(dayTwoP2());
 
